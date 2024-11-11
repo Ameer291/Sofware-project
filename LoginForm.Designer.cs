410af9c46e4fc2f779ff18c22a4de0faebb2253f
Ameer291 <ameermohamadafzal@gmail.com>
2024-11-11 00:15:46 +0000

    Add files via upload

---
+++ LoginForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿private void InitializeComponent()
+{
+    this.lblUsername = new System.Windows.Forms.Label();
+    this.lblPassword = new System.Windows.Forms.Label();
+    this.txtUsername = new System.Windows.Forms.TextBox();
+    this.txtPassword = new System.Windows.Forms.TextBox();
+    this.btnLogin = new System.Windows.Forms.Button();
+    this.chkRememberMe = new System.Windows.Forms.CheckBox();
+    this.lnkForgotPassword = new System.Windows.Forms.LinkLabel();
+    this.lnkSignUp = new System.Windows.Forms.LinkLabel();
+    this.SuspendLayout();
+
+    // Label Username
+    this.lblUsername.AutoSize = true;
+    this.lblUsername.Location = new System.Drawing.Point(50, 100);
+    this.lblUsername.Name = "lblUsername";
+    this.lblUsername.Size = new System.Drawing.Size(55, 13);
+    this.lblUsername.Text = "Username/Email:";
+
+    // TextBox Username
+    this.txtUsername.Location = new System.Drawing.Point(150, 97);
+    this.txtUsername.Name = "txtUsername";
+    this.txtUsername.Size = new System.Drawing.Size(200, 20);
+
+    // Label Password
+    this.lblPassword.AutoSize = true;
+    this.lblPassword.Location = new System.Drawing.Point(50, 140);
+    this.lblPassword.Name = "lblPassword";
+    this.lblPassword.Size = new System.Drawing.Size(53, 13);
+    this.lblPassword.Text = "Password:";
+
+    // TextBox Password
+    this.txtPassword.Location = new System.Drawing.Point(150, 137);
+    this.txtPassword.Name = "txtPassword";
+    this.txtPassword.Size = new System.Drawing.Size(200, 20);
+    this.txtPassword.UseSystemPasswordChar = true;
+
+    // Button Login
+    this.btnLogin.Location = new System.Drawing.Point(150, 180);
+    this.btnLogin.Name = "btnLogin";
+    this.btnLogin.Size = new System.Drawing.Size(200, 23);
+    this.btnLogin.Text = "Sign in";
+    this.btnLogin.UseVisualStyleBackColor = true;
+    this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+
+    // CheckBox Remember Me
+    this.chkRememberMe.AutoSize = true;
+    this.chkRememberMe.Location = new System.Drawing.Point(150, 220);
+    this.chkRememberMe.Name = "chkRememberMe";
+    this.chkRememberMe.Size = new System.Drawing.Size(94, 17);
+    this.chkRememberMe.Text = "Remember me";
+    this.chkRememberMe.UseVisualStyleBackColor = true;
+
+    // LinkLabel Forgot Password
+    this.lnkForgotPassword.AutoSize = true;
+    this.lnkForgotPassword.Location = new System.Drawing.Point(150, 250);
+    this.lnkForgotPassword.Name = "lnkForgotPassword";
+    this.lnkForgotPassword.Size = new System.Drawing.Size(86, 13);
+    this.lnkForgotPassword.TabStop = true;
+    this.lnkForgotPassword.Text = "Forgot password?";
+    this.lnkForgotPassword.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkForgotPassword_LinkClicked);
+
+    // LinkLabel Sign Up
+    this.lnkSignUp.AutoSize = true;
+    this.lnkSignUp.Location = new System.Drawing.Point(150, 280);
+    this.lnkSignUp.Name = "lnkSignUp";
+    this.lnkSignUp.Size = new System.Drawing.Size(46, 13);
+    this.lnkSignUp.TabStop = true;
+    this.lnkSignUp.Text = "Sign up";
+    this.lnkSignUp.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkSignUp_LinkClicked);
+
+    // LoginForm
+    this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+    this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+    this.ClientSize = new System.Drawing.Size(400, 350);
+    this.Controls.Add(this.lnkSignUp);
+    this.Controls.Add(this.lnkForgotPassword);
+    this.Controls.Add(this.chkRememberMe);
+    this.Controls.Add(this.btnLogin);
+    this.Controls.Add(this.txtPassword);
+    this.Controls.Add(this.lblPassword);
+    this.Controls.Add(this.txtUsername);
+    this.Controls.Add(this.lblUsername);
+    this.Name = "LoginForm";
+    this.Text = "Together Culture Login";
+    this.ResumeLayout(false);
+    this.PerformLayout();
+}
+
+
---
+++ LoginForm.cs
@@ -0,0 +1,135 @@
+﻿using Sofware_project;
+using System;
+using Microsoft.Data.SqlClient;
+using System.Windows.Forms;
+
+public partial class LoginForm : Form
+{
+    private Label lblUsername;
+    private Label lblPassword;
+    private TextBox txtUsername;
+    private TextBox txtPassword;
+    private Button btnLogin;
+    private CheckBox chkRememberMe;
+    private LinkLabel lnkForgotPassword;
+    private LinkLabel lnkSignUp;
+
+    public LoginForm()
+    {
+        InitializeComponent();
+        SetupEventHandlers();
+    }
+
+    private void InitializeComponent()
+    {
+        this.lblUsername = new Label();
+        this.lblPassword = new Label();
+        this.txtUsername = new TextBox();
+        this.txtPassword = new TextBox();
+        this.btnLogin = new Button();
+        this.chkRememberMe = new CheckBox();
+        this.lnkForgotPassword = new LinkLabel();
+        this.lnkSignUp = new LinkLabel();
+
+        // Configure Username label and textbox
+        this.lblUsername.Text = "Username/Email:";
+        this.lblUsername.Location = new System.Drawing.Point(30, 15);
+        this.txtUsername.Location = new System.Drawing.Point(150, 10);
+        this.txtUsername.Size = new System.Drawing.Size(200, 20);
+
+        // Configure Password label and textbox
+        this.lblPassword.Text = "Password:";
+        this.lblPassword.Location = new System.Drawing.Point(30, 50);
+        this.txtPassword.Location = new System.Drawing.Point(150, 45);
+        this.txtPassword.Size = new System.Drawing.Size(200, 20);
+        this.txtPassword.UseSystemPasswordChar = true;
+
+        // Configure Login button
+        this.btnLogin.Text = "Sign in";
+        this.btnLogin.Location = new System.Drawing.Point(150, 80);
+        this.btnLogin.Size = new System.Drawing.Size(200, 23);
+
+        // Configure Remember Me checkbox
+        this.chkRememberMe.Text = "Remember me";
+        this.chkRememberMe.Location = new System.Drawing.Point(150, 115);
+
+        // Set properties for Forgot Password and Sign Up links
+        this.lnkForgotPassword.Text = "Forgot password?";
+        this.lnkForgotPassword.Location = new System.Drawing.Point(150, 140);
+
+        this.lnkSignUp.Text = "Sign up";
+        this.lnkSignUp.Location = new System.Drawing.Point(150, 165);
+
+        // Adding controls to the form
+        this.Controls.Add(this.lblUsername);
+        this.Controls.Add(this.txtUsername);
+        this.Controls.Add(this.lblPassword);
+        this.Controls.Add(this.txtPassword);
+        this.Controls.Add(this.btnLogin);
+        this.Controls.Add(this.chkRememberMe);
+        this.Controls.Add(this.lnkForgotPassword);
+        this.Controls.Add(this.lnkSignUp);
+
+        this.ClientSize = new System.Drawing.Size(384, 211);
+        this.Text = "Login Form";
+        this.StartPosition = FormStartPosition.CenterScreen;
+    }
+
+    private void SetupEventHandlers()
+    {
+        this.btnLogin.Click += new EventHandler(btnLogin_Click);
+        this.lnkForgotPassword.LinkClicked += new LinkLabelLinkClickedEventHandler(lnkForgotPassword_LinkClicked);
+        this.lnkSignUp.LinkClicked += new LinkLabelLinkClickedEventHandler(lnkSignUp_LinkClicked);
+    }
+
+    private void btnLogin_Click(object sender, EventArgs e)
+    {
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\USERS\1002F\SOURCE\REPOS\SOFTWARE PROJECT\DATA\DB_TOGETHERCULTURE.MDF;Integrated Security=True";
+        if (IsValidUser(txtUsername.Text.Trim(), txtPassword.Text, connectionString))
+        {
+            MessageBox.Show("Login Successful!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            this.Hide();
+            Form1 mainForm = new Form1();  // Assuming Form1 is your main form
+            mainForm.Show();
+        }
+        else
+        {
+            MessageBox.Show("Invalid credentials, please try again.", "Login Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    private bool IsValidUser(string username, string password, string connectionString)
+    {
+        bool isValid = false;
+        using (var conn = new SqlConnection(connectionString))
+        {
+            try
+            {
+                conn.Open();
+                var query = "SELECT COUNT(1) FROM Users WHERE Username = @Username AND Password = @Password";
+                using (var cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Username", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    isValid = count == 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Database connection error: " + ex.Message, "Login Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        return isValid;
+    }
+
+    private void lnkForgotPassword_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        MessageBox.Show("Forgot Password feature not implemented yet.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+
+    private void lnkSignUp_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+        MessageBox.Show("Sign-up feature not implemented yet.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    }
+}
