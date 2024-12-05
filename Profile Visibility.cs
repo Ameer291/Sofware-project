70d1289b605f48681fc7c7be3f86bdabb7e4b8d3
Gouri1978 <gs903@student.aru.ac.uk>
2024-12-05 17:18:01 +0000

    profile visibility

---
+++ Profile Visibility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sofware_project
+{
+    public partial class Profile_Visibility : Form
+    {
+        public Profile_Visibility()
+        {
+            InitializeComponent();
+        }
+    }
+}
