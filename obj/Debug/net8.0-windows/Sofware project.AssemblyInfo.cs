48fb49cf59f8f90dc44c77f5c4fc2e37705bea6c
Ameer291 <ameermohamadafzal@gmail.com>
2024-12-06 16:04:24 +0000

    Add files via upload

--- obj/Debug/net8.0-windows/Sofware project.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Sofware project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sofware project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04150d234d9f374b3c198574fa8bd848b1cc915")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sofware project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sofware project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
